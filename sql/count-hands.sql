b4b0b91ece68dc58b213b09dd337cca7d0bb736e
Scott Lipcon <slipcon@gmail.com>
2012-02-25 08:21:51 -0500

    added average to hand count

--- sql/count-hands.sql
+++ sql/count-hands.sql
@@ -1,7 +1,18 @@
 
-SELECT players.name, count(playerhands.handid) AS count
-FROM players left outer join playerhands
-ON (playerhands.playerId=players.playerId) 
-WHERE players.stillplaying=true
-GROUP BY players.name ORDER BY count
+CREATE TEMPORARY TABLE handcount AS
+	SELECT players.name, count(playerhands.handid) AS count
+	FROM players left outer join playerhands
+	ON (playerhands.playerId=players.playerId) 
+	WHERE players.stillplaying=true
+	GROUP BY players.name ORDER BY count;
+
+\echo
+\echo This report shows the number of hands seen by player, across each
+\echo table that the player has been at so far.
+\echo
+
+SELECT * FROM handcount;
+
+SELECT avg(count) FROM handcount;
+
 
