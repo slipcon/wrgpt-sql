94d2352bbc1237f52685ab17696e6060330db827
Scott Lipcon <slipcon@gmail.com>
2012-02-27 11:44:15 -0500

    expand standings report

--- sql/standings.sql
+++ sql/standings.sql
@@ -16,3 +16,21 @@ from standings s, players p
 where p.playerid not in ( select distinct hitman from eliminations )
   AND s.playerid = p.playerid
 ORDER BY s.bankroll DESC;
+
+\echo 
+\echo Standings for those who are at their tournament high 
+\echo 
+select p.name, s.bankroll
+from standings s, players p
+where s.bankroll = s.highbankroll
+  AND s.playerid = p.playerid
+ORDER BY s.bankroll DESC;
+
+\echo 
+\echo Standings for those who are at their tournament low 
+\echo 
+select p.name, s.bankroll
+from standings s, players p
+where s.bankroll = s.lowbankroll
+  AND s.playerid = p.playerid
+ORDER BY s.bankroll DESC;
